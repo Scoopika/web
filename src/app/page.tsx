355c0fbe20f5b98b977a4bfb6662dd932650fa7a
Kais Radwan <kais.radwan.personal@gmail.com>
2024-07-23 14:13:35 -0400

    new designs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -27,8 +27,114 @@ import { MdCode, MdDataObject } from "react-icons/md";
 import { AiFillApi } from "react-icons/ai";
 import { IoMdClose } from "react-icons/io";
 import { TextGenerateEffect } from "@/components/ui/text-generate";
+import {
+  IconAdjustmentsBolt,
+  IconCloud,
+  IconCurrencyDollar,
+  IconEaseInOut,
+  IconHeart,
+  IconHelp,
+  IconRouteAltLeft,
+  IconTerminal2,
+  IconSquareRoundedCheckFilled,
+  IconLocationBolt,
+  IconDatabase,
+} from "@tabler/icons-react";
+import { cn } from "@/lib/utils";
+import { CardBody, CardContainer, CardItem } from "@/components/ui/3d-card";
 
 const words = ["multimodal", "reliable", "unique", "interactive"];
+
+const features = [
+  {
+    title: "Built for developers",
+    description:
+      "Built for engineers, developers, dreamers, thinkers and doers.",
+    icon: <IconTerminal2 />,
+  },
+  {
+    title: "Ease of use",
+    description:
+      "Just send inputs to your AI agent, much easier than sending a fetch request",
+    icon: <IconEaseInOut />,
+  },
+  {
+    title: "LLM Output validation",
+    description:
+      "Built-in validation for JSON object generation with retries and error handling",
+    icon: <IconSquareRoundedCheckFilled />,
+  },
+  {
+    title: "Real-time streaming",
+    description: "Stream text & voice responses as they're being generated",
+    icon: <IconLocationBolt />,
+  },
+  {
+    title: "Affordable",
+    description:
+      "Introduces $0 to your LLMs costs. Only pay for extra useful features",
+    icon: <IconCurrencyDollar />,
+  },
+  {
+    title: "Host anywhere",
+    description:
+      "Host your own Scoopika endpoint on your favorite cloud. memory & knowledge stores are on us ;)",
+    icon: <IconCloud />,
+  },
+  {
+    title: "Managed memory",
+    description:
+      "No more need to manage conversations with AI and store messages yourself, we got you covered!",
+    icon: <IconDatabase />,
+  },
+  {
+    title: "Open source",
+    description:
+      "Proudly open source for you and every builder to develop and use with freedom",
+    icon: <IconHeart />,
+  },
+];
+
+const Feature = ({
+  title,
+  description,
+  icon,
+  index,
+}: {
+  title: string;
+  description: string;
+  icon: React.ReactNode;
+  index: number;
+}) => {
+  return (
+    <div
+      className={cn(
+        "flex flex-col lg:border-r  py-10 relative group/feature dark:border-neutral-800",
+        (index === 0 || index === 4) && "lg:border-l dark:border-neutral-800",
+        index < 4 && "lg:border-b dark:border-neutral-800"
+      )}
+    >
+      {index < 4 && (
+        <div className="opacity-0 group-hover/feature:opacity-100 transition duration-200 absolute inset-0 h-full w-full bg-gradient-to-t from-[#717171] dark:from-[#51515151] to-transparent pointer-events-none" />
+      )}
+      {index >= 4 && (
+        <div className="opacity-0 group-hover/feature:opacity-100 transition duration-200 absolute inset-0 h-full w-full bg-gradient-to-b from-[#717171] dark:from-[#51515151] to-transparent pointer-events-none" />
+      )}
+      <div className="mb-4 relative z-10 px-10 text-neutral-600 dark:text-neutral-400">
+        {icon}
+      </div>
+      <div className="text-lg font-bold mb-2 relative z-10 px-10">
+        <div className="absolute left-0 inset-y-0 h-6 group-hover/feature:h-8 w-1 rounded-tr-full rounded-br-full bg-neutral-300 dark:bg-neutral-700 group-hover/feature:bg-white transition-all duration-200 origin-center" />
+        <span className="group-hover/feature:translate-x-2 transition duration-200 inline-block text-neutral-800 dark:text-neutral-100">
+          {title}
+        </span>
+      </div>
+      <p className="text-sm text-neutral-600 dark:text-neutral-300 max-w-xs relative z-10 px-10">
+        {description}
+      </p>
+    </div>
+  );
+};
 
 export default async function Home() {
   return (
@@ -42,7 +148,7 @@ export default async function Home() {
       </div> */}
       <Navbar session={null} active="Home" className="dark" />
 
-      <div className="relative w-full flex flex-col p-8 pt-20 md:p-12 md:pt-36 overflow-hidden rounded-md dark min-h-screen">
+      <div className="relative w-full flex flex-col p-8 pt-20 md:p-12 overflow-hidden rounded-md dark min-h-screen">
         <div className="w-full absolute inset-0 h-screen">
           <SparklesCore
             id="tsparticlesfullpage"
@@ -59,7 +165,7 @@ export default async function Home() {
             <div>
               <h1 className="text-3xl md:text-4xl lg:text-5xl font-semibold w-full text-start">
                 Build{" "}
-                <span className="bg-white">
+                <span className="moving-gradient">
                   <FlipWords
                     duration={2000}
                     words={words}
@@ -104,68 +210,85 @@ export default async function Home() {
             </div>
           </div>
           <div className="w-full flex flex-col items-center gap-4">
-            <div className="p-3 flex gap-3 border rounded-xl w-full backdrop-blur bg-accent/20 hover:border-white/20 hover:scale-105 transition-all">
-              <div className="w-10 h-10 p-2">
-                <RiRobot2Fill />
-              </div>
-              <div>
-                <div className="text-sm font-semibold">AI agents</div>
-                <div className="text-xs opacity-70">
-                  Create multimodal AI agents that can interact with your data &
-                  APIs and generate voice responses in real-time
+            <CardContainer>
+              <CardBody className="bg-gray-50 relative group/card  dark:hover:shadow-2xl dark:hover:shadow-white/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[30rem] h-auto rounded-xl p-6 border  flex flex-col items-center justify-center gap-3">
+                <CardItem>
+                  <div className="p-3 flex gap-3 border rounded-xl w-full backdrop-blur bg-accent/20 hover:border-white/20 hover:scale-105 transition-all">
+                    <div className="w-10 h-10 p-2">
+                      <RiRobot2Fill />
+                    </div>
+                    <div>
+                      <div className="text-sm font-semibold">AI agents</div>
+                      <div className="text-xs opacity-70">
+                        Create multimodal AI agents that can interact with your
+                        data & APIs and generate voice responses in real-time
+                      </div>
+                    </div>
+                  </div>
+                </CardItem>
+                <CardItem className="w-2 h-2 border rounded-full">
+                  <div></div>
+                </CardItem>
+                <CardItem className="p-3 flex gap-3 border rounded-xl w-full backdrop-blur bg-accent/20 hover:border-white/20 hover:scale-105 transition-all">
+                  <div className="w-10 h-10 p-2">
+                    <MdDataObject />
+                  </div>
+                  <div>
+                    <div className="text-sm font-semibold">
+                      Structured data generation
+                    </div>
+                    <div className="text-xs opacity-70">
+                      Generate validated JSON based on any schema from multiple
+                      data sources including text, images, audio, and URLs.
+                    </div>
+                  </div>
+                </CardItem>
+                <div className="flex flex-col gap-1">
+                  <CardItem>
+                    <div className="w-2 h-2 border rounded-full"></div>
+                  </CardItem>
                 </div>
-              </div>
-            </div>
-            <div className="w-2 h-2 bg-white rounded-full"></div>
-            <div className="p-3 flex gap-3 border rounded-xl w-full backdrop-blur bg-accent/20 hover:border-white/20 hover:scale-105 transition-all">
-              <div className="w-10 h-10 p-2">
-                <MdDataObject />
-              </div>
-              <div>
-                <div className="text-sm font-semibold">
-                  Structured data generation
+                <div className="p-3 flex gap-3 border rounded-xl w-full backdrop-blur bg-accent/20 hover:border-white/20 hover:scale-105 transition-all">
+                  <div className="w-10 h-10 p-2">
+                    <FaDatabase />
+                  </div>
+                  <div>
+                    <div className="text-sm font-semibold">
+                      Long-term memory
+                    </div>
+                    <div className="text-xs opacity-70">
+                      No more need to manage conversations history, we did it
+                      for you with managed Serverless encrypted memory stores
+                    </div>
+                  </div>
                 </div>
-                <div className="text-xs opacity-70">
-                  Generate validated JSON based on any schema from multiple data
-                  sources including text, images, audio, and URLs.
+                <div className="p-3 flex gap-3 border rounded-xl w-full backdrop-blur bg-accent/20 hover:border-white/20 hover:scale-105 transition-all">
+                  <div className="w-10 h-10 p-2">
+                    <IoLibrary />
+                  </div>
+                  <div>
+                    <div className="text-sm font-semibold">
+                      Knowledge stores
+                    </div>
+                    <div className="text-xs opacity-70">
+                      Expand your AI agents knowledge by uploading files or
+                      websites to Serverless knowledge stores served edge-close
+                      to users
+                    </div>
+                  </div>
                 </div>
-              </div>
-            </div>
-            <div className="flex flex-col gap-1">
-              <div className="w-2 h-2 bg-white rounded-full"></div>
-              <div className="w-2 h-4 bg-white rounded-full"></div>
-            </div>
-            <div className="p-3 flex gap-3 border rounded-xl w-full backdrop-blur bg-accent/20 hover:border-white/20 hover:scale-105 transition-all">
-              <div className="w-10 h-10 p-2">
-                <FaDatabase />
-              </div>
-              <div>
-                <div className="text-sm font-semibold">Long-term memory</div>
-                <div className="text-xs opacity-70">
-                  No more need to manage conversations history, we did it for
-                  you with managed Serverless encrypted memory stores
-                </div>
-              </div>
-            </div>
-            <div className="p-3 flex gap-3 border rounded-xl w-full backdrop-blur bg-accent/20 hover:border-white/20 hover:scale-105 transition-all">
-              <div className="w-10 h-10 p-2">
-                <IoLibrary />
-              </div>
-              <div>
-                <div className="text-sm font-semibold">Knowledge stores</div>
-                <div className="text-xs opacity-70">
-                  Expand your AI agents knowledge by uploading files or websites
-                  to Serverless knowledge stores served edge-close to users
-                </div>
-              </div>
-            </div>
+              </CardBody>
+            </CardContainer>
           </div>
         </div>
       </div>
 
       <div className="w-full p-8 pt-20 md:p-12 md:pt-36 dark min-h-max">
         <div className="w-full flex flex-col gap-12 lg:pl-20 lg:pr-20 min-h-max">
-          <Link href="/lost-in-the-galaxy" className="border rounded-xl bg-accent/20 p-6">
+          <Link
+            href="/lost-in-the-galaxy"
+            className="border rounded-xl bg-accent/20 p-6"
+          >
             <div className="text-xs max-w-max border border-violet-400 bg-violet-400/5 p-0.5 pl-2 pr-2 rounded-full mb-4">
               Live now
             </div>
@@ -518,6 +641,50 @@ export default async function Home() {
       </div>
 
       <div className="w-full p-8 pt-20 md:p-12 md:pt-36 dark min-h-max">
+        <div className="w-full flex flex-col gap-12 lg:pl-20 lg:pr-20 min-h-max">
+          <div className="w-full">
+            <div className="w-full flex items-center">
+              <h2
+                style={{
+                  textShadow: "10px 5px 35px rgba(255, 255, 255, .2)",
+                }}
+                className="text-3xl md:text-4xl lg:text-5xl w-full text-start w-full"
+              >
+                Why Scoopika?
+              </h2>
+            </div>
+          </div>
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 relative z-10 py-10 max-w-7xl mx-auto">
+            {features.map((feature, index) => (
+              <Feature key={feature.title} {...feature} index={index} />
+            ))}
+          </div>
+        </div>
+      </div>
+
+      <div className="w-full p-8 pt-20 md:p-12 md:pt-36 dark min-h-max">
+        <div className="w-full flex flex-col lg:flex-row lg:items-center gap-5 lg:pl-20 lg:pr-20 min-h-max">
+          <div className="w-full">
+            <div className="flex items-center gap-3 text-3xl md:text-4xl">
+              Use cases
+            </div>
+            <div className="text-sm lg:text-base opacity-70 mt-6">
+              With Scoopika you can build any AI application that can be built
+              with the OpenAI SDK, now 10 times faster, more reliable, with
+              multimodal inputs.
+            </div>
+            <div className="mt-10 flex flex-col gap-2">
+              <CheckItem title="Custom AI assistants for your website" />
+              <CheckItem title="Multimodal AI-powered search engine" />
+              <CheckItem title="Rabbit R1 (but the real software)" />
+            </div>
+          </div>
+          <div className="w-full flex items-center justify-end">
+          </div>
+        </div>
+      </div>
+
+      <div className="w-full p-8 pt-20 md:p-12 md:pt-36 dark min-h-max">
         <div className="w-full flex flex-col lg:flex-row lg:items-center gap-5 lg:pl-20 lg:pr-20 min-h-max">
           <div className="w-full">
             <div
@@ -529,9 +696,9 @@ export default async function Home() {
               <IoMdClose className="" /> No code
             </div>
             <div
-              className="flex items-center gap-3 text-3xl md:text-4xl text-green-500"
+              className="flex items-center gap-3 text-3xl md:text-4xl"
               style={{
-                textShadow: "0px 0px 5px #22c55e",
+                textShadow: "0px 0px 5px rgba(255, 255, 255, .5)",
               }}
             >
               <FaCheck className="" /> Clean code
